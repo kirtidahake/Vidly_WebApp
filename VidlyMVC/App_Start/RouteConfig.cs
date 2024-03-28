160569288b65ac8863718c7ed95e71cbf16b9882
trellis <trellis@DESKTOP-K8I4K77>
2024-03-28 18:31:47 +0530

    Add project files.

---
+++ VidlyMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace VidlyMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            //  -----------old way of creating custom routes-----------
+            //  routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleasedate" },
+            //    new { year = @"\d{4}" , month = @"\d{2}"} );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
